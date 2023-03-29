261dba8ab8323dc01883fffdeb99e801f6917221
oyelsv <oyelsv@gmail.com>
2023-03-30 01:29:18 +0200

    fix(workflows): add semantic changelog generation 2

--- release.config.js
+++ release.config.js
@@ -8,13 +8,13 @@ module.exports = {
       }
     ],
     '@semantic-release/npm',
+    '@semantic-release/github',
     [
       '@semantic-release/git',
       {
-        assets: ['docs/CHANGELOG.md'],
-        message: `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`,
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
-    '@semantic-release/github',
   ],
 };
