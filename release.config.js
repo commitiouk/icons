cfa5a1f840a7a0272ace60a11d4202ed8cfbc73c
oyelsv <oyelsv@gmail.com>
2023-03-30 02:37:26 +0200

    fix(workflows): fix for changelog

--- release.config.js
+++ release.config.js
@@ -4,6 +4,12 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],
+    [
       '@semantic-release/npm',
       {
         prepare: [
